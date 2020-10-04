c192e9ac50dd7a3f334c12bbfbc0c829e12bea0a
Anthony Candelino <32252982+anthonycandelino@users.noreply.github.com>
2020-10-04 18:40:26 -0400

    Updated layout of locations

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let aboutUs = document.getElementById("aboutUs");
-let menu = document.getElementById("menu");
-let locations = document.getElementById("locations");
+// let aboutUs = document.getElementById("aboutUs");
+// let menu = document.getElementById("menu");
+// let locations = document.getElementById("locations");
 
-$("aboutUsLink").click(function (e) { 
-    about.scrollIntoView(alignToTop);
-});
+// $("aboutUsLink").click(function (e) { 
+//     about.scrollIntoView(alignToTop);
+// });
 
-$("menuLink").click(function (e) { 
-    menu.scrollIntoView(alignToTop);
-});
+// $("menuLink").click(function (e) { 
+//     menu.scrollIntoView(alignToTop);
+// });
 
-$("locationsLink").click(function (e) { 
-    locations.scrollIntoView(alignToTop);
-});
+// $("locationsLink").click(function (e) { 
+//     locations.scrollIntoView(alignToTop);
+// });
